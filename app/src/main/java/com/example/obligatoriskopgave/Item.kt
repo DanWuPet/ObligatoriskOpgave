2872b0bdbc1353f8c83b1deec1228a9c4842fe2e
DanWuPet <danx1698@edu.easj.dk>
2023-04-18 09:30:12 +0200

    Login working

---
+++ app/src/main/java/com/example/obligatoriskopgave/Item.kt
@@ -0,0 +1,3 @@
+package com.example.obligatoriskopgave
+
+data class Item()
--- app/src/main/java/com/example/obligatoriskopgave/LoginFragment.kt
+++ app/src/main/java/com/example/obligatoriskopgave/LoginFragment.kt
@@ -1,12 +1,21 @@
 package com.example.obligatoriskopgave
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.obligatoriskopgave.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlin.math.log
 
 
 /**
@@ -15,10 +24,8 @@ import com.example.obligatoriskopgave.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,12 +41,78 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_listFragment)
+            val email = binding.editTextTextEmailAddress.text.trim().toString()
+            if (email.isEmpty()) {
+                binding.editTextTextEmailAddress.error = "No email"
+                return@setOnClickListener
+            }
+            val password = binding.editTextTextPassword.text.trim().toString()
+            if (password.isEmpty()) {
+                binding.editTextTextPassword.error = "No password"
+                return@setOnClickListener
+            }
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                        Toast.makeText(requireContext(), "Logging In", Toast.LENGTH_SHORT).show()
+                        findNavController().navigate(R.id.action_FirstFragment_to_listFragment)
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception
+                        )
+                        Toast.makeText(
+                            requireContext(), "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        updateUI(null)
+                    }
+                }
+        }
+
+        binding.buttonSecond.setOnClickListener {
+            val email = binding.editTextTextEmailAddress.text.trim().toString()
+            if (email.isEmpty()) {
+                binding.editTextTextEmailAddress.error = "No email"
+                return@setOnClickListener
+            }
+            val password = binding.editTextTextPassword.text.trim().toString()
+            if (password.isEmpty()) {
+                binding.editTextTextPassword.error = "No password"
+                return@setOnClickListener
+            }
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("APPLE", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("APPLE", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            requireContext(),
+                            "Registration failed: " + task.exception?.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
+                }
         }
     }
+
+    private fun updateUI(user: FirebaseUser?): Any {
+        Log.d("APPLE", "$user is logged in.")
+        return user.toString()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
